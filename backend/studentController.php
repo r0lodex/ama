10fc2cbf46cc2f9a1ff8a71f9bcf84c3674447b2
Nice <niceperson404@gmail.com>
2015-04-04 04:10:07 +0800

    [backend] : fix json format when return single row. return object instead of array [backend] : handle the json format send by app for post and update

--- backend/studentController.php
+++ backend/studentController.php
@@ -3,16 +3,26 @@
 switch($method) {
 	case 'GET':
 		$data['id'] = (isset($request[1])) ? $request[1] : null;
-		$sql = ($data['id'] != null) ? "SELECT * FROM student WHERE id=:id" : "SELECT * FROM student";
+		$sql = "SELECT std.id,std.name,std.ic,std.matrix,uni.name AS uniform,std.course FROM student std JOIN uniform uni ON uni.id=std.uniform";
+		$sql.= ($data['id'] != null) ? " WHERE std.id=:id" : " ";
 		$dbc = Database();
 		$qry = $dbc->prepare($sql);
 		$qry->execute($data);
 		$rows = $qry->fetchAll(PDO::FETCH_ASSOC);
 		$dbc = null;
-		echo json_encode($rows);
+		if(sizeof($rows) < 2) {
+			echo json_encode($rows[0]);
+		}else{
+			echo json_encode($rows);
+		}
 	break;
 
 	case 'POST':
+		foreach($data as $k => $v) {
+			if(is_array($v)) {
+				$data[$k] = $v[0];
+			}
+		}
 		$sql ="INSERT INTO student (name, ic, matrix, uniform, course) VALUES (:name, :ic, :matrix, :uniform, :course)";
 		$dbc = Database();
 		$qry = $dbc->prepare($sql);
@@ -21,6 +31,11 @@ switch($method) {
 	break;
 
 	case 'PUT':
+		foreach($data as $k => $v) {
+			if(is_array($v)) {
+				$data[$k] = $v[0];
+			}
+		}
 		$data['id'] = $request[1];
 		$sql ="UPDATE student SET name=:name, ic=:ic, matrix=:matrix, uniform=:uniform, course=:course WHERE id=:id";
 		$dbc = Database();
