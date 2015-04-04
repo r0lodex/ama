9caa9d93baa9f45adcf09c297aacf504cc6a914e
Irfan Radzi <irfan@spreeo.com>
2015-04-04 20:19:58 +0800

    Integrated REST for Student model.

--- source/javascripts/ama_controllers.js
+++ source/javascripts/ama_controllers.js
@@ -1,12 +1,14 @@
 angular.module('ama')
 
-// App Controllers.
-
+// == NAVIGATION CONTROLLER
+// -------------------------------
 .controller('navCTRL', function($scope, $location) {
 	$scope.isActive = function(route) {
         return route === $location.path();
     };
 })
+
+// <-- end navigation
 
 // == UNIFORM CONTROLLER
 // -------------------------------
@@ -14,59 +16,83 @@ angular.module('ama')
 	console.log('Uniform');
 })
 
+// <-- end uniform
 
 // == STUDENT CONTROLLER
 // -------------------------------
-.controller('studentCTRL', function($scope, $location, Student, ModalService) {
-	// Dapatkan senarai Student
-	$scope.students = Student.query();
+	.controller('studentCTRL', function($rootScope, $scope, $location, Student) {
 
-	// Field berdasarkan database model
-	$scope.fields = ['name', 'ic', 'matrix', 'uniform', 'course']
+		// Dapatkan senarai Student
+		$scope.students = Student.query();
 
-	// Sort table
-	$scope.sort = function(field) {
-		$scope.sort.field = field;
-		$scope.sort.order = !$scope.sort.order;
-	};
+		// Field berdasarkan database model
+		$scope.fields = ['name', 'ic', 'matrix', 'uniform', 'course']
 
-	// Default value untuk field
-	$scope.sort.field = 'name';
-	$scope.sort.order = false;
+		// Sort table
+		$scope.sort = function(field) {
+			$scope.sort.field = field;
+			$scope.sort.order = !$scope.sort.order;
+		};
 
-	// View/Edit Student
-	$scope.showStudent = function(id) {
-		$scope.theStudent = Student.get({ id:parseInt(id) })
-	};
+		$scope.sort.field = 'name';
+		$scope.sort.order = false;
 
-	$scope.newStudent = function() {
-		ModalService.showModal({
-			templateUrl: 'views/modals/student.modal',
-			controller: "studentCTRL"
-		}).then(function(modal) {
-			modal.element.modal();
-		})
-	};
+		// View/Edit Student
+		$scope.showStudent = function(id) {
+			$rootScope.newStudentForm(id, 'viewStudentCTRL');
+		};
 
-	$scope.student = new Student({
-		name 	: ['', 'text'],
-		ic 		: ['', 'text'],
-		matrix 	: ['', 'text'],
-		uniform : ['', 'number'],
-		course 	: ['', 'text']
 	})
 
-	$scope.save = function() {
-		if ($scope.newStudent.$invalid) {
-			$scope.$broadcast('record:invalid');
-		} else {
-			$scope.student.$save();
+	.controller('newStudentCTRL', function($scope, Student, Uniform, studentData) {
+
+		$scope.uniforms = Uniform.query();
+
+		$scope.student = new Student({
+			name : '',
+			ic : '',
+			matrix : '',
+			uniform : 1, // Defaults to 1
+			course : ''
+		})
+
+		$scope.addStudent = function() {
+			if ($scope.new_student_form.$invalid) {
+				$scope.$broadcast('record:invalid');
+			} else {
+				$scope.student.$save();
+				alert('Student successfully created.');
+				// Resorts to refreshing due to out of scope.
+				window.location.reload();
+			}
 		}
-	}
+	})
 
-	$scope.deleteStudent = function(id) {}
+	.controller('viewStudentCTRL', function($scope, Student, Uniform, studentData) {
+		$scope.editing = true;
+		$scope.uniforms = Uniform.query();
+		$scope.student = studentData;
 
-})
+		$scope.addStudent = function() {
+			if ($scope.new_student_form.$invalid) {
+				$scope.$broadcast('record:invalid');
+			} else {
+				$scope.student.$update();
+				alert('Student data successfully updated.');
+				window.location.reload();
+				// Resorts to refreshing due to out of scope.
+			}
+		}
+
+		$scope.deleteStudent = function() {
+			var a = confirm('Are you sure you want to delete this student?');
+			if (a) {
+				$scope.student.$delete();
+				window.location.reload();
+			}
+		}
+	})
+// <-- end student
 
 
 // == USER CONTROLLER
@@ -74,3 +100,5 @@ angular.module('ama')
 .controller('userCTRL', function($scope) {
 	console.log('Users');
 })
+
+// <-- end user
