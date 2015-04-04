29d1493a1d5eb19d2fbbd297ffa9d9c36ae9880e
Nice <niceperson404@gmail.com>
2015-04-05 00:28:50 +0800

    [backend] - expand unifrom controller.

--- backend/uniformController.php
+++ backend/uniformController.php
@@ -2,17 +2,54 @@
 
 switch($method) {
 	case 'GET':
-		$data['id'] = (isset($request[1])) ? $request[1] : null;
-		$sql = ($data['id'] != null) ? "SELECT * FROM uniform WHERE id=:id" : "SELECT * FROM uniform";
-		$dbc = Database();
-		$qry = $dbc->prepare($sql);
-		$qry->execute($data);
-		$rows = $qry->fetchAll(PDO::FETCH_ASSOC);
-		$dbc = null;
-		if($data['id'] != null) {
-			$out = $rows[0];
+		if(isset($request[2])) {
+			switch($request[2]) {
+				case 'absent':
+				$data = array('id'=>$request[1]);
+					$sql = "SELECT stdn.name, stdn.matrix, stdn.course, (COUNT(abst.student_id)) AS absent_count, 
+					(COUNT(abst.student_id) * unfm.credit) AS absent_credit  FROM absent abst 
+					JOIN student stdn ON stdn.id=abst.student_id JOIN uniform unfm ON unfm.id=abst.uniform_id 
+					WHERE abst.uniform_id=:id GROUP BY abst.student_id";
+					$dbc = Database();
+					$qry = $dbc->prepare($sql);
+					$qry->execute($data);
+					$rows = $qry->fetchAll(PDO::FETCH_ASSOC);
+					$dbc = null;
+					if(sizeof($rows) > 1) {
+						$out = $rows;
+					}else{
+						$out = $rows[0];
+					}
+				break;
+
+				case 'record':
+					$data = array('a'=>$request[1]);
+					$sql = "SELECT * FROM student WHERE uniform=:a";
+					$dbc = Database();
+					$qry = $dbc->prepare($sql);
+					$qry->execute($data);
+					$rows = $qry->fetchAll(PDO::FETCH_ASSOC);
+					$dbc = null;
+					if(sizeof($rows) > 1) {
+						$out = $rows;
+					}else{
+						$out = $rows[0];
+					}
+				break;
+			}
 		}else{
-			$out = $rows;
+			$data['id'] = (isset($request[1])) ? $request[1] : null;
+			$sql = ($data['id'] != null) ? "SELECT * FROM uniform WHERE id=:id" : "SELECT * FROM uniform";
+			$dbc = Database();
+			$qry = $dbc->prepare($sql);
+			$qry->execute($data);
+			$rows = $qry->fetchAll(PDO::FETCH_ASSOC);
+			$dbc = null;
+			if($data['id'] != null) {
+				$out = $rows[0];
+			}else{
+				$out = $rows;
+			}
 		}
 		echo json_encode($out);
 	break;
