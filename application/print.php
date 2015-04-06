366f413c70e56c55d0b46a17065f04e507925fcd
Irfan Radzi <irfan@spreeo.com>
2015-04-06 17:30:29 +0800

    Print feature

---
+++ application/print.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AMA App Dashboard</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../source/stylesheets/ama.css">
+	<link rel="stylesheet" type="text/css" href="../source//vendor/angular/directives/loading-bar.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="table-responsive">
+			<div class="page-header">
+				<h1>
+					<small>Absent Report</small><br>
+					PENGAKAP
+				</h1>
+			</div>
+			<table class="table table-bordered table-condensed">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Name</th>
+						<th>Matrix</th>
+						<th>Class</th>
+						<th>Credit</th>
+						<th>Absent Count</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Abu Tausi Jaiha</td>
+						<td>YMCA123456</td>
+						<td>Kelas Menjahit</td>
+						<td>12</td>
+						<td>2</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Abu Tausi Jaiha</td>
+						<td>YMCA123456</td>
+						<td>Kelas Menjahit</td>
+						<td>12</td>
+						<td>2</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Abu Tausi Jaiha</td>
+						<td>YMCA123456</td>
+						<td>Kelas Menjahit</td>
+						<td>12</td>
+						<td>2</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
+<script type="text/javascript">
+	document.addEventListener('DOMContentLoaded', function(event) {
+		window.print();
+	})
+</script>
